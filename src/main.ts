64187ba2d2dbe7e85de8515dceea330df36e2517
santiago5087 <santiago5087@hotmail.com>
2026-04-23 00:32:45 -0500

    chore: CORS config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { envs } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Habilitar CORS para permitir peticiones desde el frontend Angular
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   // Habilitar validaciones globales
   app.useGlobalPipes(
     new ValidationPipe({
